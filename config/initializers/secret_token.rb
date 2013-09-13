cb54b6d7ddfd0fe3db9f1e67b0549a275bd3da10
Saxon Fletcher <saxon@projuce.com>
2013-09-13 15:28:27 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recruitment::Application.config.secret_key_base = '72f393a7836fbc1a97dff16c4cb12ab98011a8e067fc2ea0c39fd04ad2a6387edeb5021a73d310d229667bb2beafb83cfbb3ba80499c88fa7bb03e1836f1c843'
