90fc2a63ebf09334b58eef29d86fb52ea8c36b1b
Saxon Fletcher <saxon@projuce.com>
2013-10-06 20:07:30 +1000

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Recruitment::Application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   resources :users
 
