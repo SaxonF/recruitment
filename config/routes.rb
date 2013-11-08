3e1295da8b999aee0dd1d202c6db6a99367a3cc5
Saxon Fletcher <saxon@projuce.com>
2013-11-08 19:16:11 +1000

    add user micro posts

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Recruitment::Application.routes.draw do
 
   resources :users
 
+  resources :microposts, only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
